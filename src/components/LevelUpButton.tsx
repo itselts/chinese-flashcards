4a9045d5407ad3e13efdc311eec14e26f2b1ac64
Elton Shi <elton1@live.com.au>
2023-07-24 22:10:49 +1000

    complete counter with reset at 9

---
+++ src/components/LevelUpButton.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const LevelUpButton = () => {
+    // Styling
+    const buttonStyle = {
+        backgroundColor: "green",
+        color: "white",
+        padding: "10px 20px",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer",
+        };
+
+    // State to keep track of the count
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        const newCount = count === 10 ? 0 : count + 1
+        setCount(newCount)
+    }
+    return <button style={buttonStyle} onClick={handleClick}> Level {count} </button>
+}
+
+export default LevelUpButton
