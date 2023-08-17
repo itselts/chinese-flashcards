1c6e8e4e712c7c588b51680c3aa6f791bdeaa0b1
Elton Shi <elton1@live.com.au>
2023-08-17 17:48:47 +1000

    Add a bit of styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import zhongwen from '/zhong-wen.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,15 +8,18 @@ import LevelUpButton from './components/LevelUpButton'
 function App() {
   return (
     <>
-      <div>
-        {/* <a href="https://vitejs.dev" target="_blank">
+      <nav>
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-      </div>
-      <h1>Chinese Flashcards</h1>
+        </a>
+        <a href="https://en.wikipedia.org/wiki/Xi_Jinping" target="_blank">
+          <img src={zhongwen} className='logo react' alt='ZhongWen' />
+        </a>
+      </nav>
+      <h1 className='inter-tight-header header-style'>Chinese Flashcards</h1>
       <div className="card">
         <LevelUpButton/>
       </div>
--- src/components/LevelUpButton.tsx
+++ src/components/LevelUpButton.tsx
@@ -1,22 +1,12 @@
 import { useState } from 'react';
 
 const LevelUpButton = () => {
-    // Styling
-    const buttonStyle = {
-        backgroundColor: "green",
-        color: "white",
-        padding: "10px 20px",
-        border: "none",
-        borderRadius: "5px",
-        cursor: "pointer",
-        marginRight: "10px"
-        };
 
     // State to keep track of the count
     const [count, setCount] = useState(1);
 
     function handleClick() {
-        const newCount = count === 9 ? 1 : count + 1
+        const newCount = count === 5 ? 1 : count + 1
         setCount(newCount)
     };
 
@@ -25,10 +15,10 @@ const LevelUpButton = () => {
     };
 
     return (
-        <>
+        <div>
             <button type="button" onClick={handleClick} className="btn btn-primary"> Level {count} </button>
             <button type="button" onClick={handleReset} className="btn btn-secondary"> Reset </button>
-        </>
+        </div>
     );
 }
 
