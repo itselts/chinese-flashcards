d50afde645439ab45bdf75cca5136a6a6854cfd2
Elton Shi <elton1@live.com.au>
2023-12-20 21:23:55 +1100

    remove congrats page footer bug

--- src/routes/congratulations.tsx
+++ src/routes/congratulations.tsx
@@ -5,7 +5,6 @@ export default function Congratulations() {
         <div>
             <h1>Congratulations!</h1>
             <Link to={'/chinese-flashcards'} className="btn btn-danger">Home</Link>
-            <footer>When you return to the home page, refresh the browser if you want to replay the same level... (Bug to be fixed)</footer>
         </div>
     )
 }
