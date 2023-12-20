cdf868261221ebb6ed9522e7f8be41812202b115
Elton Shi <elton1@live.com.au>
2023-12-20 21:22:52 +1100

    characters and topics formatted

--- src/characters.ts
+++ src/characters.ts
@@ -1,13 +1,8 @@
 export default [
-    [{"char": "\u4e00", "pinyin": "yi", "tone": "1", "hint": "Number one"}, {"char": "\u53e3", "pinyin": "kou", "tone": "3", "hint": "Mouth"}, {"char": "\u5973", "pinyin": "nu", "tone": "3", "hint": "Woman"},  
-        {"char": "\u2f08", "pinyin": "ren", "tone": "2", "hint": "people"}, {"char": "\u5b89", "pinyin": "an", "tone": "1", "hint": "peace"}, {"char": "\u5b50", "pinyin": "zi", "tone": "3", "hint": "child"},
-        {"char": "\u592a", "pinyin": "tai", "tone": "4", "hint": "too"}, {"char": "\u592a", "pinyin": "zi", "tone": "4", "hint": "word"}],
-    [{"char": "\u4e07", "pinyin": "wan", "tone": "4", "hint": "10000"}, {"char": "\u4ebf", "pinyin": "yi", "tone": "4", "hint": "100000"}, {"char": "\u96f6", "pinyin": "ling", "tone": "2", "hint": "0"},
-        {"char": "\u6208", "pinyin": "ge", "tone": "1", "hint": "spear"}],
-    [{"char": "\u4e09", "pinyin": "san", "tone": "1", "hint": "Number three"}],
-    [{"char": "\u56db", "pinyin": "si", "tone": "4", "hint": "Number four"}],
-    [{"char": "\u4e94", "pinyin": "wu", "tone": "3", "hint": "Number five"}]    
-];
-
-
-
+    [{"char": "\u4e00", "pinyin": "yi", "tone": "1", "hint": "Number one"}, {"char": "\u4ebf", "pinyin": "yi", "tone": "4", "hint": "100000000"}, 
+        {"char": "\u96f6", "pinyin": "ling", "tone": "2", "hint": "0"}], 
+    [{"char": "\u8dd1", "pinyin": "pao", "tone": "3", "hint": "run"}],  
+    [{"char": "\u767d", "pinyin": "bai", "tone": "2", "hint": "white"}],
+    [{"char": "\u8089", "pinyin": "rou", "tone": "4", "hint": "meat"}],
+    [{"char": "\u8c22", "pinyin": "xie", "tone": "4", "hint": "thank you"}]
+]
--- src/topics.ts
+++ src/topics.ts
@@ -3,5 +3,5 @@ export default [
     ["Basic Verbs", '\uD83C\uDFC3', 'Simple action words!'], 
     ["Colours", '\uD83D\uDD34', 'At least know the primary colours...'], 
     ["Menu", '\uD83C\uDF72', 'To help you when ordering food'], 
-    ["General Vocabulary", '\uD83D\uDCD6', 'General characters needed to read basic things']
+    ["General Vocabulary", '\uD83D\uDCD6', 'General characters needed to read basic things.']
 ]
